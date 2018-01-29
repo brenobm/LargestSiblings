954c59db4247bd8450d5f24f7c2fe47fbd2650a6
Breno Batista Machado <brenomachado@gmail.com>
2018-01-29 15:28:22 -0200

    Implementation

--- LargestSiblings/Program.cs
+++ LargestSiblings/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LargestSiblings
 {
@@ -10,6 +8,38 @@ namespace LargestSiblings
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please insert a integer to found its largest sibling:");
+            string input = Console.ReadLine();
+            int n = 0;
+
+            if (int.TryParse(input, out n))
+            {
+                Console.WriteLine($"Largest sibling: {CalculteLargestSibling(n)}");
+            }
+            else
+            {
+                Console.WriteLine("Input isn't a valid integer");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int CalculteLargestSibling(int n)
+        {
+            // Convert to list of chars
+            IList<char> numbers = n.ToString().ToList();
+
+            // Sort from greather to lower
+            numbers = numbers.OrderByDescending(c => c).ToList();
+
+            // Join the sorted list and convert to long to avoid overflow if it became more than 2147483647
+            long largestSibling = long.Parse(string.Join("", numbers));
+
+            // Verify if it greather than 100.000.000
+            if (largestSibling > 100000000)
+                return -1;
+
+            return (int)largestSibling;
         }
     }
 }
